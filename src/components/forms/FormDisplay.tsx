8a80fe2e65c7c8ebda0e1536c06fa438c419165c
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-11-14 18:28:18 -0500

    add components

---
+++ src/components/forms/FormDisplay.tsx
@@ -0,0 +1,3 @@
+export default function FormDisplay() {
+	return <h1>This is the form dispaly component</h1>;
+}
---
+++ src/components/forms/FormFields.tsx
@@ -0,0 +1,3 @@
+export default function FormFields() {
+	return <h1>This is a form field component</h1>;
+}
