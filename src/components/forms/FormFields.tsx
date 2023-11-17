a9ce87177835ef5cecd5dbc7a857c2ccca057041
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-11-16 22:47:26 -0500

    complete form field component

--- src/components/forms/FormFields.tsx
+++ src/components/forms/FormFields.tsx
@@ -1,3 +1,89 @@
+import { useState } from 'react';
+
 export default function FormFields() {
-	return <h1>This is a form field component</h1>;
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	return (
+		<div className='space-y-8'>
+			<form className='space-y-4'>
+				<div className='bg-gray-200 space-x-4 p-2'>
+					<label htmlFor='firstName'>First Name</label>
+					<input
+						type='text'
+						id='firstName'
+						onChange={(e) => {
+							setFirstName(e.target.value);
+						}}
+					/>
+				</div>
+
+				<div className='bg-gray-200 space-x-4 p-2'>
+					<label htmlFor='lastName'>Last Name</label>
+					<input
+						type='text'
+						id='lastName'
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+				</div>
+
+				<div className='bg-gray-200 space-x-4 p-2'>
+					<label htmlFor='email'>Email</label>
+					<input
+						type='text'
+						id='email'
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+
+				<div className='bg-gray-200 space-x-4 p-2'>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						id='password'
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+
+				<div className='bg-gray-200 space-x-4 p-2'>
+					<label htmlFor='confirmPassword'>Confirm Password</label>
+					<input
+						type='password'
+						id='confirmPassword'
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+				</div>
+			</form>
+			<div>
+				<h1> Your Form Data </h1>
+				<div className='space-x-4'>
+					<span>First Name: </span>
+					<span>{firstName}</span>
+				</div>
+
+				<div className='space-x-4'>
+					<span>Last Name: </span>
+					<span>{lastName}</span>
+				</div>
+
+				<div className='space-x-4'>
+					<span>Email: </span>
+					<span>{email}</span>
+				</div>
+
+				<div className='space-x-4'>
+					<span>Password: </span>
+					<span>{password}</span>
+				</div>
+
+				<div className='space-x-4'>
+					<span>Confirm Password: </span>
+					<span>{confirmPassword}</span>
+				</div>
+			</div>
+		</div>
+	);
 }
